7f5814519710da29a6ebca042ce943f974192527
Thiago Medeiros <fmedeiros.edge@scml.pt>
2022-12-21 17:44:31 +0000

    Private field restriction set

--- src/test/java/upper/IUpperTest.java
+++ src/test/java/upper/IUpperTest.java
@@ -4,17 +4,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.projecttest.upper.IUpper;
 import com.example.projecttest.upper.IUpperRepository;
+import com.example.projecttest.upper.LookUtils;
 import com.example.projecttest.upper.UpperImpl;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 class IUpperTest {
 
     @Mock
-    IUpperRepository repository;
+    private IUpperRepository repository;
 
     @InjectMocks
     final IUpper iUpper;
@@ -37,6 +39,17 @@ class IUpperTest {
         Mockito.when(repository.findRepository()).thenReturn("James");
 
         assertEquals("ABJames", iUpper.upper("ab"));
+    }
+
+    @Test
+    void testStaticClass() {
+//        try (MockedStatic<LookUtils> utilities = Mockito.mockStatic(LookUtils.class)) {
+//            utilities.when(LookUtils::getName)
+//                .thenReturn("NameForTest");
+//        }
+
+            assertEquals("NameForTest", LookUtils.getName());
+
     }
 
 }
