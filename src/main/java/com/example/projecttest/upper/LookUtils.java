aac040654b92d1b83a6b338e8a96e198fbe1d445
Thiago Medeiros <fmedeiros.edge@scml.pt>
2022-12-21 17:33:38 +0000

    Static class test

---
+++ src/main/java/com/example/projecttest/upper/LookUtils.java
@@ -0,0 +1,11 @@
+package com.example.projecttest.upper;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class LookUtils {
+
+    public static String getName() {
+        return "StaticClassTest";
+    }
+}
